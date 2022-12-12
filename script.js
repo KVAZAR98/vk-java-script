3515fdc3233580df768a39d61b69f4d647e2fcde
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-12 11:38:29 +0300

    112

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-src="https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js"
-vkBridge.send('VKWebAppInit');
-vkBridge.send('VKWebAppGetEmail')
-  .then((data) => { 
-    if (data.email) {
-      // Доступ к электронной почте получен
-    }
-  })
-  .catch((error) => {
-    // Ошибка
-    console.log(error);
-  });
-
-
 window.alert("Ghb");
