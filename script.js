17e58225f90428945b37e4aef8932bc216fcc860
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-12 11:12:51 +0300

    1

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// Отправка события
-vkBridge.send('VKWebAppGetEmail')
-  .then((data) => {
-    if (data.result) {
-      // Обработка события в случае успеха
-      console.log(data.email);
-    } else {
-      // Ошибка
-    }
-  })
-  .catch((error) => {
-    // Обработка события в случае ошибки
-    console.log(error);
-  });
+alert(a);
