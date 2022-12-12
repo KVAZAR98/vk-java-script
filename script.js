23ad127b1cbd84936993a66e5f79a438617b8f3c
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-12 11:25:14 +0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+bridge.send('VKWebAppGetEmail')
+  .then((data) => { 
+    if (data.email) {
+      // Доступ к электронной почте получен
+    }
+  })
+  .catch((error) => {
+    // Ошибка
+    console.log(error);
+  });
+
+
 window.alert(a);
